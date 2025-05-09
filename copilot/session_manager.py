6b6407765cf4dfaa59bd8557e5050085812ee179
Veerav Naidu <veeravn@gmail.com>
2025-05-09 00:14:11 -0500

    update table name

--- copilot/session_manager.py
+++ copilot/session_manager.py
@@ -1,18 +1,20 @@
 import os
 import json
-from azure.data.tables import TableServiceClient, UpdateMode
+from azure.data.tables import TableClient, UpdateMode
 
-TABLE_NAME = os.getenv("AZ_TABLE_NAME", "CopilotSessions")
+TABLE_NAME = "CopilotSessions"
 CONNECTION_STR = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
-svc_client = TableServiceClient.from_connection_string(CONNECTION_STR)
-table_client = svc_client.get_table_client(TABLE_NAME)
 
 def get_user_session(user_id: str) -> dict:
-    try:
-        entity = table_client.get_entity(partition_key="user", row_key=user_id)
-        return json.loads(entity.get("session_data", "{}"))
-    except:
-        return {"history": []}
+    
+    with TableClient.from_connection_string(
+        conn_str=CONNECTION_STR, table_name=TABLE_NAME
+        ) as table_client:
+            try:
+                entity = table_client.get_entity(partition_key="user", row_key=user_id)
+                return json.loads(entity.get("session_data", "{}"))
+            except:
+                return {"history": []}
 
 def save_user_session(user_id: str, session_data: dict) -> None:
     entity = {
@@ -20,5 +22,8 @@ def save_user_session(user_id: str, session_data: dict) -> None:
         "RowKey": user_id,
         "session_data": json.dumps(session_data)
     }
-    table_client.upsert_entity(entity, mode=UpdateMode.MERGE)
+    with TableClient.from_connection_string(
+       conn_str=CONNECTION_STR, table_name=TABLE_NAME
+       ) as table_client:
+            table_client.upsert_entity(entity, mode=UpdateMode.MERGE)
 
